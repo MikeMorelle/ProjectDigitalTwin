b0652f8e9d14026f95d6c802684e991590c7478d
MikeMorelle <michael.morg21@gmail.com>
2026-07-31 23:39:15 +0200

    Delete outdated pred_main_final directory

--- pred_main_final/db/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS sensor_data (
-    engine_id INT NOT NULL,
-    cycle INT NOT NULL,
-    ops JSONB NOT NULL,
-    sensors JSONB NOT NULL,
-    timestamp TIMESTAMPTZ DEFAULT NOW(),
-    PRIMARY KEY (engine_id, cycle)
-);
